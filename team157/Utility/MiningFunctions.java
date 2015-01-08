50bc3a36f288b67c6f4bb7d4c59363dabb29d8b5
Minh Tue Vo <mvo@box.com>
2015-01-07 23:24:52 -0800

    add mining functions 2

--- team157/Utility/MiningFunctions.java
+++ team157/Utility/MiningFunctions.java
@@ -5,11 +5,10 @@ package Utility;
  */
 public class MiningFunctions {
 
-	// TODO: for efficiency's sake, we might maintain a list of ore values & locations
-    public static Point simpleRatio(Point currentLocation, int[][] grid)
-    {
-        float minRatio = 0;
-        Point minPoint = new Point(0, 0);
+    // TODO: for efficiency's sake, we might maintain a list of ore values & locations
+    public static Point simpleRatio(Point currentLocation, int[][] grid) {
+        float maxRatio = 0;
+        Point maxPoint = new Point(0, 0);
 
         int numRows = grid.length;
         int numCols = grid[0].length;
@@ -17,12 +16,29 @@ public class MiningFunctions {
         for (int i = 0; i < numRows; i++)
             for (int j = 0; j < numCols; j++)
             {
-                if ((float)grid[i][j]/currentLocation.distance(i, j) > minRatio)
+                if ((float)grid[i][j]/currentLocation.distance(i, j) > maxRatio)
                 {
-                    minRatio = (float)grid[i][j]/currentLocation.distance(i, j);
-                    minPoint = new Point(i, j);
+                    maxRatio = (float)grid[i][j]/currentLocation.distance(i, j);
+                    maxPoint = new Point(i, j);
                 }
             }
-        return minPoint;
+        return maxPoint;
+    }
+
+    public static Point simpleRatio(Point currentLocation, Point[] oreLocations, int[] oreValues){
+        float maxRatio = 0;
+        Point maxPoint = new Point(0, 0);
+
+        int n = oreLocations.length;
+
+        for (int i = 0; i < n; i++) {
+            if ((float)oreValues[i]/currentLocation.distance(oreLocations[i]) > maxRatio)
+            {
+                maxRatio = (float)oreValues[i]/currentLocation.distance(oreLocations[i]);
+                maxPoint = oreLocations[i];
+            }
+        }
+
+        return maxPoint;
     }
 }
