e3c5469a007cc208250f82234caa58d149f1bcdf
Minh Tue Vo <mvo@box.com>
2015-01-10 15:25:31 -0800

    adding risk map and modifying mining functions

--- teams/team157/Utility/MiningFunctions.java
+++ teams/team157/Utility/MiningFunctions.java
@@ -1,14 +1,23 @@
 package Utility;
+import battlecode.common.MapLocation;
 
 /**
  *
  */
 public class MiningFunctions {
 
-    // TODO: for efficiency's sake, we might maintain a list of ore values & locations
-    public static Point simpleRatio(Point currentLocation, int[][] grid) {
+    protected static int distance(int x1, int y1, int x2, int y2) {
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    }
+
+    protected static int distance(MapLocation loc1, MapLocation loc2)
+    {
+        return Math.abs(loc1.x - loc2.x) + Math.abs(loc1.y - loc2.y);
+    }
+
+    public static MapLocation simpleRatio(MapLocation currentLocation, int[][] grid) {
         float maxRatio = 0;
-        Point maxPoint = new Point(0, 0);
+        MapLocation maxPoint = new MapLocation(0, 0);
 
         int numRows = grid.length;
         int numCols = grid[0].length;
@@ -16,29 +25,28 @@ public class MiningFunctions {
         for (int i = 0; i < numRows; i++)
             for (int j = 0; j < numCols; j++)
             {
-                if ((float)grid[i][j]/currentLocation.distance(i, j) > maxRatio)
+                if ((float)grid[i][j]/distance(currentLocation.x, currentLocation.y, i, j) > maxRatio)
                 {
-                    maxRatio = (float)grid[i][j]/currentLocation.distance(i, j);
-                    maxPoint = new Point(i, j);
+                    maxRatio = (float)grid[i][j]/distance(currentLocation.x, currentLocation.y, i, j);
+                    maxPoint = new MapLocation(i, j);
                 }
             }
         return maxPoint;
     }
 
-    public static Point simpleRatio(Point currentLocation, Point[] oreLocations, int[] oreValues){
+    public static MapLocation simpleRatio(MapLocation currentLocation, MapLocation[] oreLocations, int[] oreValues){
         float maxRatio = 0;
-        Point maxPoint = new Point(0, 0);
+        MapLocation maxPoint = new MapLocation(0, 0);
 
         int n = oreLocations.length;
 
         for (int i = 0; i < n; i++) {
-            if ((float)oreValues[i]/currentLocation.distance(oreLocations[i]) > maxRatio)
+            if ((float)oreValues[i]/distance(currentLocation, oreLocations[i]) > maxRatio)
             {
-                maxRatio = (float)oreValues[i]/currentLocation.distance(oreLocations[i]);
+                maxRatio = (float)oreValues[i]/distance(currentLocation, oreLocations[i]);
                 maxPoint = oreLocations[i];
             }
         }
-
         return maxPoint;
     }
 }
---
+++ teams/team157/Utility/RiskMap.java
@@ -0,0 +1,19 @@
+package team157.Utility;
+
+import battlecode.common.MapLocation;
+
+/**
+ * This map helps to evaluate the risk of any position
+ */
+public class RiskMap {
+
+	public RiskMap()
+	{
+
+	}
+
+	public void addEnemySighting(MapLocation location)
+	{
+
+	}
+}
