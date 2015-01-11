7f67fca16025df3af6f64c90637fcfe7e806caec
Minh Tue Vo <mvo@box.com>
2015-01-10 20:53:25 -0800

    adding ore class

---
+++ teams/team157/Utility/Ore.java
@@ -0,0 +1,38 @@
+package team157.Utility;
+
+import battlecode.common.MapLocation;
+
+/**
+ *
+ */
+public class Ore {
+
+    private int value;
+    private MapLocation location;
+
+    public Ore(int value, MapLocation location)
+    {
+        this.value = value;
+        this.location = location;
+    }
+
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+
+    public int getValue()
+    {
+        return this.value;
+    }
+
+    public MapLocation getLocation()
+    {
+        return this.location;
+    }
+
+    public void setLocation(MapLocation location)
+    {
+        this.location = location;
+    }
+}
