d5a8b017e9d3882a62ca8547150db23a09338fb8
Minh Tue Vo <mvo@box.com>
2015-01-10 16:12:51 -0800

    adding risk map

--- teams/team157/Utility/RiskMap.java
+++ teams/team157/Utility/RiskMap.java
@@ -1,19 +1,38 @@
 package team157.Utility;
 
 import battlecode.common.MapLocation;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * This map helps to evaluate the risk of any position
+ *
+ * Unit sighting: decrease over time, exponentially
+ * Building sighting: remain constant
  */
 public class RiskMap {
 
+	private ArrayList<MapLocation> sightings;
+	private float radius = 5;
+
 	public RiskMap()
 	{
-
+		this.sightings = new ArrayList<MapLocation>();
 	}
 
-	public void addEnemySighting(MapLocation location)
+	public void addSighting(MapLocation location)
 	{
+		this.sightings.add(location);
+	}
 
+	public float query(MapLocation loc)
+	{
+		for (Iterator<MapLocation> i = sightings.iterator(); i.hasNext(); ) {
+			MapLocation sighting = i.next();
+			if (loc.distanceSquaredTo(sighting) > radius*radius) {
+				return 1;
+			}
+		}
+		return 0;
 	}
 }
