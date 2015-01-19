e37c390a3b44b3534e01c2980bd669bb61ac7238
Minh Tue Vo <mvo@box.com>
2015-01-18 23:33:20 -0800

    try to launch missiles

--- teams/team157/Launcher.java
+++ teams/team157/Launcher.java
@@ -51,16 +51,10 @@ public class Launcher extends MovableUnit {
      */
     public static void launchMissile(Direction dir0) throws GameActionException {
         if(rc.isCoreReady() && rc.getTeamOre() >= RobotType.MISSILE.oreCost) {
-            int dirint0 = dir0.ordinal();
-            // TODO: more efficient launching procedures
-            for(int offset : offsets) {
-                int dirint = (dirint0+offset+8)%8;
-                if(rc.canSpawn(directions[dirint],RobotType.MISSILE)) {
-                    // Not sure if I can do this
-                    Missile.target = target;
-                    rc.spawn(directions[dirint],RobotType.MISSILE);
-                    break;
-                }
+            if(rc.canSpawn(dir0,RobotType.MISSILE)) {
+                // Not sure if I can do this
+                Missile.target = target;
+                rc.spawn(dir0, RobotType.MISSILE);
             }
         }
     }
